4b504c5ef7a09e12c3cdd08280322592c349ae84
Func <Funcer@outlook.com>
2025-12-07 05:13:16 +0800

    IcuCollation: Remove redundant code setting NUMERIC_COLLATION
    Added tests to assert that NUMERIC_COLLATION is set from the locale
    for different versions of PHP.
    
    Change-Id: I9699e8f807ee2c1b75b5afb239ac811d921dded7

---
+++ tests/phpunit/includes/collation/IcuCollationTest.php
@@ -0,0 +1,25 @@
+<?php
+
+
+/**
+ * @covers \IcuCollation
+ */
+class IcuCollationTest extends MediaWikiLangTestCase {
+
+	/**
+	 * @dataProvider numericAttributeProvider
+	 */
+	public function testNumericAttribute( string $collation, int $expected ) {
+		$col = $this->getServiceContainer()->getCollationFactory()->makeCollation( $collation );
+		$reflection = new ReflectionProperty( IcuCollation::class, 'mainCollator' );
+		$collator = $reflection->getValue( $col );
+		$this->assertEquals( $expected, $collator->getAttribute( Collator::NUMERIC_COLLATION ) );
+	}
+
+	public static function numericAttributeProvider() {
+		return [
+			[ 'uca-default', Collator::OFF ],
+			[ 'uca-default-u-kn', Collator::ON ],
+		];
+	}
+}
